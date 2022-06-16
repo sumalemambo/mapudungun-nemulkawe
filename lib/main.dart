ff7d16c89c6832d0b47f7c3b05e5bd568344054a
Chopan <francisco.manriquezn@usm.cl>
2022-06-16 14:51:00 -0400

    La barra de estado se mantiene oculta, la única orientación es la vertical

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,20 @@
-import 'package:app/providers/favorites_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:app/providers/favorites_provider.dart';
 import 'package:app/components/nav.dart';
 import 'package:provider/provider.dart';
 
-/*
-import 'package:flutter/services.dart' show ByteData, rootBundle;
-import 'package:csv/csv.dart';
-import 'package:app/models/database_helper.dart';
-import 'package:app/models/word_model.dart';
-
-void loadDict() async {
-  String path = 'csv/Mapuzuguletuain_2019.csv';
-  final _rawData = await rootBundle.loadString(path);
-  List<List<dynamic>> rowsAsListOfValues = const CsvToListConverter(fieldDelimiter: ';').convert(_rawData);
-  rowsAsListOfValues = rowsAsListOfValues.sublist(1, rowsAsListOfValues.length);
-
-  for (final e in rowsAsListOfValues) {
-    Word word = Word(theme: e[0], isUnderTheme: e[1],
-        word: e[2], translation: e[3], definition: e[5],
-        conjugation: e[6], declensions: e[7], examples: e[8],
-        pronunciation: e[9]);
-    await DatabaseHelper.instance.create(word);
-  }
-}
- */
 
 // Este Bucket almacena la Scroll Position del Diccionario y los Favoritos
 final bucket = PageStorageBucket();
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  // Ocultar la barra de estado superior
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  // La única orientación posible para la app es vertical, no horizontal
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(
       MultiProvider(
         providers: [

f72daef56b040a78253235c3c563e489a8cc804d
Chopan <francisco.manriquezn@usm.cl>
2022-06-16 11:35:19 -0400

    Se agregó diccionario a SliverScreen

--- lib/screens/sliver_screen/components/sliver_screen.dart
+++ lib/screens/sliver_screen/components/sliver_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:app/database/database_helper.dart';
 import 'package:app/models/word_model.dart';
+import 'package:provider/provider.dart';
+import 'package:app/providers/favorites_provider.dart';
 import 'main.dart';
 import 'search_bar.dart';
 import 'verb_conjugator.dart';
+
+
 
 class SliverScreen extends StatelessWidget {
   const SliverScreen({Key? key}) : super(key: key);
@@ -14,11 +18,35 @@ class SliverScreen extends StatelessWidget {
       future: DatabaseHelper.selectAll(Word.table),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          var wordList = snapshot.data!;
-          return CustomScrollView(
-            slivers: const [
-              SliverAppBar(),
-            ],
+          final list = snapshot.data! as List<Map<String, dynamic>>;
+          final wordList = Word.fromList(list);
+          return Card(
+            margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+            elevation: 4.0,
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  pinned: true,
+                  floating: true,
+                  expandedHeight: 160.0,
+
+                ),
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                      (context, i) {
+                        return Card(
+                          child: ListTile(
+                            leading: Text(wordList[i].theme),
+                            title: Text(wordList[i].word),
+                            subtitle: Text(wordList[i].translation),
+                            trailing: _FavoriteButton(word: wordList[i]),
+                          ),
+                        );
+                      }
+                  )
+                ),
+              ],
+            ),
           );
         }
         else {
@@ -28,3 +56,43 @@ class SliverScreen extends StatelessWidget {
     );
   }
 }
+
+class _FavoriteButton extends StatelessWidget {
+  final Word word;
+
+  const _FavoriteButton({required this.word});
+
+  @override
+  Widget build(BuildContext context) {
+
+    var isInFavorites = context.select<FavoritesProvider, bool>(
+            (favorites) => favorites.itemIds.contains(word.id)
+    );
+
+    return IconButton(
+      onPressed: isInFavorites
+          ?  () {
+        var favorites = context.read<FavoritesProvider>();
+        print(favorites.item);
+        print(favorites.itemIds);
+        favorites.remove(word.id!);
+      }
+          : () {
+        // If the item is not in cart, we let the user add it.
+        // We are using context.read() here because the callback
+        // is executed whenever the user taps the button. In other
+        // words, it is executed outside the build method.
+        var favorites = context.read<FavoritesProvider>();
+        favorites.add(word.id!);
+      },
+      icon: isInFavorites
+          ? const Icon(
+          Icons.favorite,
+          color: Colors.pink,
+          semanticLabel: 'ADDED'
+      )
+          : const Icon(Icons.favorite, color: Colors.grey,),
+    );
+  }
+}
+
